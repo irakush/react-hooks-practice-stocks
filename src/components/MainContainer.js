7847be3a5c48f123b5390330ca20c936719007eb
Igor Rakush <irakush@aol.com>
2023-12-20 00:39:37 -0500

    render all stocks

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const URL = "http://localhost:3001/stocks"
+
+  const [stocksArray, setStocksArray] = useState([])
+
+  useEffect(() => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(setStocksArray) // .then(data => setStocksArray(data))
+  }, [])
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocksArray={stocksArray} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocksArray }) {
   return (
     <div>
       <h2>Stocks</h2>
       {/* render stock list here*/}
+      { stocksArray.map(stock => <Stock stock={stock} />) }
     </div>
   );
 }
